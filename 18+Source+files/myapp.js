73182e79bbf35efdb4ef1aad0c8953badc0b174c
SphereSRYN <sphere.sryn@gmail.com>
2022-01-24 18:34:12 +0900

    splice

--- 18+Source+files/myapp.js
+++ 18+Source+files/myapp.js
@@ -10,15 +10,34 @@ var array = [
 array[0] = "New string";
 
 // Methods
-array.shift(); // Delete's first element
-array.pop(); // Delete's last element
+//array.shift(); // Delete's first element 선입선출
+array.pop(); // Delete's last element 후입선출
 
 // Add new values to the beginning of the array
-array.unshift( "new", "values" );
+array.unshift( "new", "new values" ); //선입
 
 // Add new values to the end of the array
-array.push( "ending", "values" );
+array.push( "ending", "ending values" );//후입
 
+array.forEach(element => {
+    console.log(element);
+});
+console.log("============");
+for (const key in array) {
+    if (array.hasOwnProperty.call(array, key)) {
+        const element = array[key];
+       // console.log(element.valueOf());
+      //  console.log(array);
+    }
+}
+console.log(array[7].valueOf());
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element)
+// }
 // Splice can do anything remember
 // splice( point, delete, add values... )
-array.splice( 2, 2, "new elemets", [], {}, 200, 300, function(){} );
+array.splice( 2, 0, "new elemets", [], {}, 200, 300, function(){} );
+array.forEach(element => {
+    console.log(element);
+});
